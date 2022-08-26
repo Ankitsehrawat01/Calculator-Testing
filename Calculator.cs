34560290dcf213957ca14e88fed2d9186452ca8e
Ankitsehrawat01 <sehrawata328@gmail.com>
2022-08-26 14:52:52 +0530

    Unit testing of Calculator

---
+++ Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_Demo
+{
+    public class Calculator
+    {
+        public int Addition(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public int Subtraction(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        public int Multiplication(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        public int Division(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Calculator_Demo
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Calculator calculatorobj = new Calculator();
+            int result = calculatorobj.Addition(2, 2);
+            Console.WriteLine(result);
+            int result1 = calculatorobj.Subtraction(2, 2);
+            Console.WriteLine(result1);
+            int result2 = calculatorobj.Multiplication(2, 2);
+            Console.WriteLine(result2);
+            int result3 = calculatorobj.Division(2, 2);
+            Console.WriteLine(result3);
+        }
+    }
+}
